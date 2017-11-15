116db071513a5f16fdddd7fda71a7f24d9d9695a
Artem Rebryk <dedok8099@gmail.com>
2017-11-15 10:00:03 +0200

    calendar for payments

---
+++ public/js/onloadHandler3.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+    $('.demo').daterangepicker({
+        "autoApply": true,
+        "ranges": {
+            'Today': [moment(), moment()],
+            'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+            'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+            'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+            'This Month': [moment().startOf('month'), moment().endOf('month')],
+            'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+        },
+        "alwaysShowCalendars": true,
+        "opens": "center"
+    });
+};
