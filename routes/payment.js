a1040dec6ed75117dcb46e7bcb7b1c8ab06dea2f
Artem Rebryk <dedok8099@gmail.com>
2017-11-16 17:24:21 +0200

    web payment

--- routes/payment.js
+++ routes/payment.js
@@ -83,6 +83,13 @@ router.post('/payments/add:id?', upload.any(), function(req, res, next){
   });
 }, function(program, req, res, next){
   var item = {};
+  var date = new Date(req.body.date)
+  var now = new Date;
+  now.setHours(0, 0, 0, 0);
+  var status = 'done';
+  if(date > now){
+    status = 'pending';
+  }
   // console.log(program);
   if(req.files[0]){
     item.image = req.files[0].filename;
@@ -92,6 +99,7 @@ router.post('/payments/add:id?', upload.any(), function(req, res, next){
   item.type = req.body.type;
   item.userID = req.params.id;
   item.program = program;
+  item.status = status;
   next(item);
 }, function(item, req, res, next){
   var data = new Payment(item);
@@ -103,6 +111,63 @@ router.post('/payments/add:id?', upload.any(), function(req, res, next){
     } else {
       res.sendStatus(200);
     }
+  });
+});
+
+
+
+//--------------------------edit-------------------------
+router.post('/payments/edit/payment_:id?', upload.any(), function(req, res, next){
+  var id = req.params.id;
+  Payment.findById(id).then(function(doc){
+    if(req.files[0]){
+      doc.image = req.files[0].filename;
+    };
+    if(req.body.sum){
+      doc.sum = req.body.sum;
+    };
+    if(req.body.date){
+      doc.date = req.body.date;
+    };
+    if(req.body.type){
+      doc.type = req.body.type;
+    };
+    if(req.body.status){
+      doc.status = req.body.status;
+    };
+    doc.save(function (err) {
+      if (err) {
+        console.log(err);
+        res.sendStatus(403);
+      } else {
+        res.send({
+          _id: doc._id,
+          date: doc.date,
+          sum: doc.sum,
+          type: doc.type,
+          status: doc.status,
+          program: doc.program
+        });
+      }
+    });
+  });
+});
+
+//--------------------------delete-------------------------
+router.post('/payments/delete/payment_:id?', function(req, res, next){
+  var id = req.params.id;
+  Payment.findById(id).then(function(doc){
+    if (doc.image){
+      fs.unlink('./public/img/upload/house/' + doc.image, function (err){
+        if (err) throw err;
+        console.log('successfully deleted - ' + doc.image);
+      });
+    };
+    next(id);
+  })
+}, function(id, req, res, next){
+  Payment.findByIdAndRemove(id).then(function(){
+    res.sendStatus(200);
   });
 });
 
